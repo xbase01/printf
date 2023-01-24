a025f898a7ec7116d5e8cf27d2a6f4c91c333a69
lizzyraesetja <lizzraesetja2@gmail.com>
2023-01-24 13:40:19 -0800

    params.c

--- params.c
+++ params.c
@@ -6,20 +6,20 @@
  * @ap: the argument pointer
  * Return: void
  */
-void init_params(params_t *params, va_list ap)
+void init_params(params_t *params, va_list ap)
 {
-	params->unsign = 0;
+	params->unsign = 0;
 
-	params->plus_flag = 0;
-	params->space_flag = 0;
-	params->hashtag_flag = 0;
-	params->zero_flag = 0;
-	params->minus_flag = 0;
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
 
-	params->width = 0;
-	params->precision = UINT_MAX;
+	params->width = 0;
+	params->precision = UINT_MAX;
 
-	params->h_modifier = 0;
-	params->l_modifier = 0;
+	params->h_modifier = 0;
+	params->l_modifier = 0;
 	(void)ap;
 }
